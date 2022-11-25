1db62f82795a4a7efd750564cd51bc2d7260397a
Timur Ulanovsky <ulanovskytimur@gmail.com>
2022-11-25 19:00:16 +0200

    Routing. Forms.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { OrderListComponent } from './order-list/order-list.component';
+import { AddProuctComponent } from './add-order/add-product.component';
+
+const routes: Routes = [
+  { path: '', component: OrderListComponent},
+  { path: 'add-order', component: AddProuctComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
